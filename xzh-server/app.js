ea95186e1c5ebfc13d26cd0ecb3f2d92c9e24f65
xzh <xzh990520@gmail.com>
2026-06-16 16:49:05 +0800

    finish level 7 challenge: xzh-server

---
+++ xzh-server/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+    
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {    
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is a about page');
+});
+
+app.get('/api/time', (req, res) => {
+    res.json({
+        time: new Date().toLocaleTimeString(),
+        date: new Date().toLocaleDateString()
+    });
+});
+
+app.get('/api/greeting', (req, res) => {
+    const name = req.query.name || 'World'
+    res.json({greeting: 'Hello' + name + "!"})
+})
+
+app.listen(port, () => {
+    console.log("The server is running");
+})
+    
