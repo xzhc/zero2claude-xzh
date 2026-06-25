ebd4b284cdf7008383ce8635749aac9edc6b099a
xzh <xzh990520@gmail.com>
2026-06-25 15:28:27 +0800

    feat: add joke display in my server

--- my-server/app.js
+++ my-server/app.js
@@ -23,6 +23,17 @@ app.get('/api/time', (req, res) => {
     });
 });
 
+app.get('/api/joke', async (req,res) => {
+    try {
+        const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+        const joke = await response.json();
+        res.json({setup: joke.setup, punchline: joke.punchline});;
+    } catch (error) {
+        res.status(500).json({error: 'Failed to fetch joke'});
+    }    
+})
+
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 })
